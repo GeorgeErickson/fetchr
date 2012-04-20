8ad66e4ee64c11b8214a6d85cb8e09ecfee322b1
George Erickson <george55@mit.edu>
2012-04-20 17:58:39 -0400

    initializing

---
+++ fetchr.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import clint
+
+
+def main():
+    libraries = clint.args.not_files
+    print libraries
+
+if __name__ == '__main__':
+    main()
+
