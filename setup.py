39980bdaa3cefc28a3fa96029f38f06864d37599
George Erickson <george55@mit.edu>
2012-04-21 21:00:36 -0400

    Have the proper github url

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as requirements:
     requires = map(lambda r: r.strip(), requirements.readlines())
 
-__version__ = '0.0.0'
+version = '0.0.0'
 
 dist = setup(
     name = 'fetchr',
-    version = __version__,
+    version = version,
     packages = find_packages(),
     package_data = {'fetchr': ['data/config.yaml']},
     include_package_data = True,
     description = 'A tool to load files from the internet',
     author = 'George Erickson',
     author_email = 'george55@mit.edu',
-    download_url = 'https://github.com/GeorgeErickson/fetchr/zipball/master',
+    download_url = 'https://github.com/GeorgeErickson/fetchr/zipball/v%s' % version,
     install_requires = requires,
     entry_points = {
         'console_scripts': [
