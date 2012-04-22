092ba7901388560dc1e47efe77c730ccd6e9a27f
George Erickson <george55@mit.edu>
2012-04-21 20:54:58 -0400

    donna tag a version

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 import os
 import sys
+import fetchr
 from setuptools import setup, find_packages
 
 #dynamically read dependencies from file
 with open('requirements.txt') as requirements:
     requires = map(lambda r: r.strip(), requirements.readlines())
 
+__version__ = '0.0.0'
+
 dist = setup(
     name = 'fetchr',
-    version ='0.0.1',
+    version = __version__,
     packages = find_packages(),
-    package_data={'fetchr': ['data/config.yaml']},
+    package_data = {'fetchr': ['data/config.yaml']},
     include_package_data = True,
     description = 'A tool to load files from the internet',
     author = 'George Erickson',
     author_email = 'george55@mit.edu',
     download_url = 'https://github.com/GeorgeErickson/fetchr/zipball/master',
+    install_requires = requires,
     entry_points = {
         'console_scripts': [
             'fetchr = fetchr.fetchr:main'
